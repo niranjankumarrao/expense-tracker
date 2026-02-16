4f833fda51eaa72a60c7ed1d2790ecb5c9fa8399
niranjankumarrao <150656746+niranjankumarrao@users.noreply.github.com>
2026-02-16 09:33:19 +0530

    Update login API URL to use HTTPS

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
 
     try {
       const res = await axios.post(
-        "http://expense-tracker-icua.onrender.com/api/auth/login",
+        "https://expense-tracker-icua.onrender.com/api/auth/login",
         form
       );
 
